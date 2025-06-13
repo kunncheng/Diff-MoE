d94a46d1d9ac60686a01b88daafdaddd6c4c8e80
Kun Cheng <113667477+kunncheng@users.noreply.github.com>
2025-06-13 14:43:53 +0800

    Update load.py

---
+++ utils/load.py
@@ -0,0 +1,17 @@
+import importlib
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    if config.get("params"):
+        return get_obj_from_str(config["target"])(**config.get("params", dict()))
+    else:
+        config['n_embed'] = config['embed_dim']
+        return get_obj_from_str(config["model_target"])(**config)
